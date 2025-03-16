7303337a5009b10c20d7b4a2be1da0c8c53bdfea
Igor Stepanov <stipyk1309@gmail.com>
2025-03-16 15:22:45 +0300

    feat: attack functional with health bar

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
+function getRandomValue(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min
+}
+
 const app = Vue.createApp({
 	data() {
-		return {}
+		return {
+			monsterHealth: 100,
+			playerHealth: 100,
+		}
 	},
+	computed: {
+		monsterBarStyles() {
+			return { width: this.monsterHealth + '%' }
+		},
+		playerBarStyles() {
+			return { width: this.playerHealth + '%' }
+		},
+	},
+	methods: {
+		attackMonster() {
+			const maxDamage = 12
+			const minDamage = 5
 
-	methods: {},
+			this.monsterHealth -= getRandomValue(minDamage, maxDamage)
+			this.attackPlayer()
+		},
+		attackPlayer() {
+			const maxDamage = 15
+			const minDamage = 8
+
+			this.playerHealth -= getRandomValue(minDamage, maxDamage)
+		},
+	},
 })
 
 app.mount('#game')
